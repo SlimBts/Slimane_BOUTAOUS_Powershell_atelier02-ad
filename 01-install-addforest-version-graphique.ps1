cdd8cba21dd636366b45f933be68ce9532a6988a
unknown <Administrateur@ISITECH.LOCAL>
2026-05-29 17:54:29 +0200

    Modification apportées sur le fichier 01-install-addforest-version-graphique

--- 01-install-addforest-version-graphique.ps1
+++ 01-install-addforest-version-graphique.ps1
@@ -5,22 +5,19 @@ Add-Type -AssemblyName System.Drawing
 $form = New-Object System.Windows.Forms.Form
 $form.Text = "Configuration Serveur" 
 $form.StartPosition = "CenterScreen"
-$form.Size = New-Object System.Drawing.Size(300, 250) # Ajouté pour donner de l'espace aux boutons
 
 #Construction des boutons
 
 #1. Construction du bouton DHCP
 $buttonsDhcp = New-Object System.Windows.Forms.Button
 $buttonsDhcp.Text = "Ajout DHCP"
-$buttonsDhcp.Location = New-Object System.Drawing.Point(50, 20) # Corrigé : Positionnement initial
-$buttonsDhcp.Size = New-Object System.Drawing.Size(180, 30)     # Corrigé : Taille pour afficher le texte
 $buttonsDhcp.Add_Click({
         [string]$scopeName = 'IsitechLocal'
         [string]$start = '192.168.79.40'
         [string]$stop = '192.168.79.90'
         [string]$mask = '255.255.255.0'
         [int]$day = 8
-        [string]$dns = [Microsoft.VisualBasic.Interaction]::InputBox( # Corrigé : Majuscule à VisualBasic
+        [string]$dns = [Microsoft.VisualBasic.Interaction]::InputBox(
         "Veuillez saisir le DNS", "DHCP", "8.8.8.8"
 )
         [string]$domain = 'isitech.local'
@@ -35,12 +32,10 @@ $buttonsDhcp.Add_Click({
 # 2. Construction du bouton Renommer
 $buttonRename = New-Object System.Windows.Forms.Button
 $buttonRename.Text = "Renommer Serveur"
-$buttonRename.Location = New-Object System.Drawing.Point(50, 70) # Corrigé : Positionnement pour éviter la superposition
-$buttonRename.Size = New-Object System.Drawing.Size(180, 30)     # Corrigé : Taille pour afficher le texte
 $buttonRename.Add_Click({
 
     #On remplace Read-Host par une InputBox (partie graphique)
-    [string]$nomSRV = [Microsoft.VisualBasic.Interaction]::InputBox( # Corrigé : Remplacement de [Microsoft.Visual.Interaction] par [Microsoft.VisualBasic.Interaction]
+    [string]$nomSRV = [Microsoft.VisualBasic.Interaction]::InputBox(
         "veuillez saisir le nouveau Nom : ", "Renommer"
     )
     Rename-Computer -NewName $nomSRV -Restart
@@ -49,8 +44,6 @@ $buttonRename.Add_Click({
 # 3. Construction du bouton AD DS
 $buttonAD = New-Object System.Windows.Forms.Button
 $buttonAD.Text = "Installation AD DS"
-$buttonAD.Location = New-Object System.Drawing.Point(50, 120)    # Ajusté : Positionnement
-$buttonAD.Size = New-Object System.Drawing.Size(180, 30)         # Corrigé : Taille pour afficher le texte
 $buttonAD.Add_Click({
     # Saisie du mot de passe via InputBox pour l'interface graphique
     [string]$passwordNonCrypte = [Microsoft.VisualBasic.Interaction]::InputBox("Veuillez saisir le mot de passe DSRM", "AD DS")
@@ -67,7 +60,7 @@ $buttonAD.Add_Click({
 })
 
 # Ajout des contrôles au formulaire
-$form.Controls.Add($buttonsDhcp) # Corrigé : Le nom de la variable était $buttonsDhcp au lieu de $buttonDhcp
+$form.Controls.Add($buttonsDhcp)
 $form.Controls.Add($buttonRename)
 $form.Controls.Add($buttonAD)
 
